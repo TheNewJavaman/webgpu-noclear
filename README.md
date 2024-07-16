57bc8f7b5a4af97114555b31eb30f6133f6d63c1
Gabriel Pizarro <gpizarro@javaman.net>
2024-07-16 10:30:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webgpu-leak
+
+- Enable flag on Linux: <https://github.com/gpuweb/gpuweb/wiki/Implementation-Status>
---
+++ index.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("canvas");
+
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.");
+}
+
+const device = await adapter.requestDevice();
+
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+    device: device,
+    format: canvasFormat,
+});
+
+const encoder = device.createCommandEncoder();
+
+const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+        view: context.getCurrentTexture().createView(),
+        loadOp: "load",
+        storeOp: "store",
+    }]
+});
+
+// pass.end();
+
+// device.queue.submit([encoder.finish()]);
